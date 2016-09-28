5fe854e586c4562a01fefecc902a7ed898912ff6
aleksandervevle <aleksander.vevle@lyse.net>
2016-09-28 20:41:31 +0200

    want:1 now works. Added generated JSON

--- php/getInfo.php
+++ php/getInfo.php
@@ -1,32 +1,17 @@
-<?
+<?php
 //Written by Aleksander Vevle
 //Work in Progress
-//This is not done or tested yet
-
+//Want:1 now works
 $want=$_POST["want"];
-//1 Get all schoolnames
-//2 Get all schools with all freedays
-//3 One school with days off
-//4 Get all info about a school
-$mysqli = new mysqli("localhost","root","","skoleruter");
+//1 Get all schools with all freedays (most used)
+//2 One school with days off
+//3 Get all info about a school
+$mysqli = mysqli_connect("localhost","root","","skoleruter");
+mysqli_set_charset($mysqli,"utf8"); //Set charset
 if($mysqli->connect_error) {
 	die("Error, please try again");
 }
-if($want==1){
-  $sqlgetOrderNumber="select navn from skole s order by navn;";
-  $result=$mysqli->query($sqlgetOrderNumber);
-  if($result->num_rows !=0){
-    while($row=$result->fetch_assoc()){
-      $name=$row['navn'];
-    }
-  }
-  else{
-    echo "Error, please try again";
-}
-$mysqli->close();
-  }
-else if ($want==2){//Ikke ferdig
-  $temp=array()
+if ($want==1){//Get all schools with all freedays (most used)
   $laerer=array();
   $elev=array();
   $sfo=array();
@@ -34,56 +19,54 @@ else if ($want==2){//Ikke ferdig
   $result=$mysqli->query($sqlgetOrderNumber);
   if($result->num_rows !=0){
     while($row=$result->fetch_assoc()){
-      $name=$row['navn'];
-      array_push($temp,$name);
-      array_push($laerer,$name);
-      array_push($elev,$name);
-      array_push($sfo,$name);
+      $name=utf8_encode($row['navn']);
+				if(strpos($name,'SFO') !== false){//Er sfo
+					$sfo[$name]=array();
+				}
+				else{
+					$laerer[$name]=array();
+		      $elev[$name]=array();
+				}
     }
   }
   else{
     echo "Error, please try again";
   }
 
-  $sqlgetOrderNumber="select s.navn, f.dato, f.ikke_for_ansatte,f.grunn from skole s natural join fri f sort by s.navn ;";
-  //$temp=array();//New array
-  $result=$mysqli->query($sqlgetOrderNumber);
-  if($result->num_rows !=0){
-    while($row=$result->fetch_assoc()){
-      $name=$row['navn'];
-      for($temp as $n){
-        if($n==$name){
-          $fri=$row['navn'];
-          $ikkeAnsatte=$row['ikke_for_ansatte'];
-          $grunn=$row['grunn']
-          if(strpos($name, 'sfo') !== false){//Er ikke sfo
-            array_push($elev[$name],array($fri,$grunn))
-            if($ikkeAnsatte==0){
-                array_push($laerer[$name],array($fri,$grunn))
-            }
-          }
-          else{
-            array_push($sfo[$name],array($fri,$grunn))
-          }
-
-        }
-      }
+  $sqlgetOrderNumber="select s.navn n, f.dato d, f.ikke_for_ansatte ika,f.grunn g from skole s, fri f where s.ID=f.skoleID  order by n ;";
+	if ($result=mysqli_query($mysqli,$sqlgetOrderNumber))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+			$navn=utf8_encode($row[0]);
+			$dato=$row[1];
+			$ikkeAnsatte=$row[2];
+			$grunn=utf8_encode($row[3]);
+			if(strpos($navn,'SFO') !== false){//Er sfo
+			array_push($sfo[$navn],array($dato,$grunn));
+			}
+			else{
+				array_push($elev[$navn],array($dato,$grunn));
+				if($ikkeAnsatte==0){
+						array_push($laerer[$navn],array($dato,$grunn));
+				}
+			}
     }
-  }
-  else{
-    //echo "Error: " . $sqlgetOrderNumber . "<br>" . $mysqli->error;
-    echo "Error, please try again";
-  }
-  $res = array($elev,$laerer,$sfo);
+  // Free result set
+  mysqli_free_result($result);
+}
+
+  $res = array("elev"=>$elev,"lærer"=>$laerer,"sfo"=>$sfo);
   $jsonstring= json_encode($res);
   $mysqli->close();
-
+	echo $jsonstring;
 }
-else if ($want==3){//Ikke ferdig
+else if ($want==2){//Ikke ferdig
 //TODO
 $mysqli->close();
 }
-else if ($want==4){//Ikke ferdig
+else if ($want==3){//Ikke ferdig
 //TODO
 $mysqli->close();
 }
