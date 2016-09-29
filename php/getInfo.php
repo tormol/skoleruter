d02ddbf70b4fbda35adaf29782291fa6265217d4
aleksandervevle <aleksander.vevle@lyse.net>
2016-09-29 21:33:21 +0200

    want:2 now works

--- php/getInfo.php
+++ php/getInfo.php
@@ -2,7 +2,12 @@
 //Written by Aleksander Vevle
 //Work in Progress
 //Want:1 now works
+//Want:2 now works
 $want   = $_POST["want"];
+$skole="";
+if($want!=1){
+	$skole=$_POST["school"];
+}
 //1 Get all schools with all freedays (most used)
 //2 One school with days off
 //3 Get all info about a school
@@ -72,9 +77,82 @@ if ($want == 1) { //Get all schools with all freedays (most used)
     $jsonstring = json_encode($res);
     $mysqli->close();
     echo $jsonstring;
-} else if ($want == 2) { //Ikke ferdig
-    //TODO
-    $mysqli->close();
+} else if ($want == 2) {
+
+	if (strpos($skole, 'SFO') !== false) { //Er sfo
+		$sqlgetOrderNumber = "select s.navn n, f.dato d,f.grunn g from skole s, fri f where s.navn=\"$skole\" and s.ID=f.skoleID  order by n ;";
+		$sfo=array();
+		$sfo[$skole] = array();
+		if ($result = mysqli_query($mysqli, $sqlgetOrderNumber)) {
+				// Fetch one and one row
+				while ($row = mysqli_fetch_row($result)) {
+						$navn        = utf8_encode($row[0]);
+						$dato        = $row[1];
+						$grunn       = utf8_encode($row[2]);
+						if($grunn==""){
+							$grunn="Ukjent";
+						}
+								array_push($sfo[$navn], array(
+										$dato,
+										$grunn
+								));
+
+				}
+				// Free result set
+				mysqli_free_result($result);
+				$res        = array(
+						"sfo" => $sfo
+				);
+				$jsonstring = json_encode($res);
+				$mysqli->close();
+				echo $jsonstring;
+		}
+		else{
+			echo "error";
+		}
+	} else {
+		$laerer            = array();
+    $elev              = array();
+		$laerer[$skole] = array();
+		$elev[$skole]   = array();
+		$sqlgetOrderNumber = "select s.navn n, f.dato d, f.ikke_for_ansatte ika,f.grunn g from skole s, fri f where s.navn=\"$skole\" and s.ID=f.skoleID  order by n ;";
+		if ($result = mysqli_query($mysqli, $sqlgetOrderNumber)) {
+				// Fetch one and one row
+				while ($row = mysqli_fetch_row($result)) {
+						$navn        = utf8_encode($row[0]);
+						$dato        = $row[1];
+						$ikkeAnsatte = $row[2];
+						$grunn       = utf8_encode($row[3]);
+						if($grunn==""){
+							$grunn="Ukjent";
+						}
+								array_push($elev[$navn], array(
+										$dato,
+										$grunn
+								));
+								if ($ikkeAnsatte == 0) {
+										array_push($laerer[$navn], array(
+												$dato,
+												$grunn
+										));
+
+						}
+				}
+				// Free result set
+				mysqli_free_result($result);
+				$res        = array(
+						"elev" => $elev,
+						"lærer" => $laerer
+				);
+				$jsonstring = json_encode($res);
+				$mysqli->close();
+				echo $jsonstring;
+		}
+		else{
+			echo "error";
+		}
+	}
+
 } else if ($want == 3) { //Ikke ferdig
     //TODO
     $mysqli->close();
