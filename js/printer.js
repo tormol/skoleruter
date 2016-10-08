c8e347aa4cf30a6b44fa2a661badae72f89f5b5d
Helge Bjorland <helgejo@gmail.com>
2016-10-08 20:50:32 +0200

    dummy filterDates funksjon i printer denne er TODO av tabell teamet

--- js/printer.js
+++ js/printer.js
@@ -51,3 +51,9 @@ function addskolevalg(skolenavn){
      $("#skolevalg").append(valg);
      $("#skolevalg").trigger("chosen:updated");
 }
+
+function filterDates(period){
+  // TODO: Implementer filtrering på dato av tabell
+  // perioden er dictionary av typen {start:dd/mm/yyyy, end:dd/mm/yyyy}
+  //start er første dag og end er siste dag i perioden som er valgt
+}
