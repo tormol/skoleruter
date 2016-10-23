56c2fe61ab72f7e1147305bf8f73a5c95272dbd3
Yngve Lindseth <Lindseth94@outlook.com>
2016-10-23 18:38:00 +0000

    Updated printer.js

--- js/printer.js
+++ js/printer.js
@@ -1,64 +1,61 @@
-//For fast refrence to current date
-var year = new Date().getFullYear();
-var month = new Date().getMonth() + 1;
-var day = new Date().getDate();
-var test;
-function printDays(dagsObjekt, start, end) {
+/* An attempt to make Printer a lot more readable and structured */
 
-    var full = "";
-    var units = "<td></td>";
+// Pool of Global Variables, anything here needs to be commented and reasoned for
 
-    $("#q").empty()
-    $('#units').empty()
-    //$('#units').append($("<td></td>")); //Appends an empty field for the corner
-    //console.log(start)
-    var skoleNr = 1;
-    $.each(dagsObjekt, function(skolenavn, SkoleObj) { //For hver skole
+var activeSchools = null; // this is requred by prints(), it also needs to  be saved between multiple print() calls
+var dateRange = null; // used by printRow
+var typeList = null; // used by printRow TODO Add default value
+var SkoleObject = null;
+function printT() {
 
-      var row = "<tr><td>" + skolenavn + "</td>";
+    prints(SkoleObject)
+    selectSchools(activeSchools);
+}
+function prints(data) {
+    if (SkoleObject == null) SkoleObject = data;
 
-      addskolevalg(skolenavn);
-      //$('#q').append('<tr><td>' + skolenavn + '</td></tr>')
-      $.each(SkoleObj, function(Aar, AarObj) { //For hvert år
-        $.each(AarObj, function(Mnd, MndObj) { //For hver Mnd
-       //   if(Mnd != 0 && MndObj.length > 0){ //Hopper over tilfellet når måned = 0
-            for(var Dag = 1; Dag <= daysInMonth(Mnd, Aar); Dag++){ //Dag = tallet; MndObj[Dag] = Beskjed
-              //Sjekker om datoen er størren enn dagens dato
-              if(dateInRange(Aar, Mnd, Dag, year, month, day, start, end)) {
-              //console.log("triggered")
-                if(skoleNr == 1){
-                  units += "<td class=topBar>" + getTopText(Dag, Mnd, Aar, MndObj[Dag]) + "</td>";
+    /* Main printer controller */
+    printInit();
+
+    var full = "", units = "";
+    var First = true;
+    
+    $.each(SkoleObject, function(skolenavn, SkoleObj) { // itterer gjennom alle skolene
+
+        chosenAddSkoleValg(skolenavn); // Legger skolenavnet til dropdown lista over skoler
+        var row = "<tr><td>" + skolenavn + "</td>";
+
+        $.each(SkoleObj, function(Aar, AarObj) { // For hvert år:
+            $.each(AarObj, function(Mnd, MndObj) { // For hver måned:
+                for(var Dag = 1; Dag <= daysInMonth(Mnd, Aar); Dag++){ // Går gjennom alle dagene i en måned
+                    //Sjekker om datoen er valid
+                    if(dateInRange(Aar, Mnd, Dag)) {
+                        //Legger til den rette enheten
+                        if(First) units += getTopText(Dag, Mnd, Aar, MndObj[Dag]);
+                        //Legger til dagen
+                        if (MndObj[Dag] == undefined) row += "<td></td>";
+                        else row += "<td class=" + cssTypes(MndObj[Dag][1]) + "></td>";
+                    }
                 }
-
-                if (MndObj[Dag] != undefined) {
-                  row += "<td class=no_school></td>";
-                }
-                else row += "<td></td>";
-              }
-            }
-         // }
+            });
         });
-      });
-      row += "</tr>";
-      full += row;
-      //$('#q').append(row);
-      skoleNr++;
+        // legger til rekken
+        row += "</tr>";
+        full += row;
+        if(First) First = false;
     });
     $('#units').append(units);
-    document.getElementById("q").innerHTML = full;
+    $('#q').append(full);
 
     var table = $("#fixTable");
     table.tableHeadFixer({"left" : 1});
-
     var parent = table.parent();
     parent.focus();
     // This cannot be done at $(document).ready() because the menu changes size.
     setHeight(parent);
     $(window).resize(function() {
-        setHeight(parent);
+      setHeight(parent);
     })
-
-    test = dagsObjekt
 }
 
 function getTopText(dag, mnd, aar, bes){
@@ -83,9 +80,54 @@ function getTopText(dag, mnd, aar, bes){
    if(temp == "sommerferie") temp =     "som.fer.";
    text += temp;
  }
-  return text;
+  return "<td class=topBar>" + text + "</td>";
 }
 
+function printInit() {
+    /* Initilize the table for printing,
+     * Must delete previous entries for redraw, or will just add to table */
+    $("#q").empty()
+    $('#units').empty()
+    $('#units').append($("<td></td>")); //Appends an empty field for the corner
+}
+
+function chosenAddSkoleValg(skolenavn){
+    var valg = $("<option></option>").text(skolenavn);
+    $("#skolevalg").append(valg);
+    $("#skolevalg").trigger("chosen:updated");
+}
+function dateInRange(Aar, Mnd, Dag){
+    /* takes in a day, if a date range is not set: Set start date to TODAY and no end date
+     * check if date is in range.
+     * */
+
+    if (dateRange == null) {
+        // Henter dagens dato
+
+        var yearToday = new Date().getFullYear();
+        var monthToday = new Date().getMonth() + 1;
+        var dayToday = new Date().getDate();
+        if((Aar == yearToday && Mnd >= monthToday) || Aar > yearToday){
+
+            if(Mnd == monthToday) {
+                return (Dag >= dayToday) ? true : false
+            }
+            return true
+        }
+
+    }   else {
+
+        var fDate,lDate,cDate;
+        // CHANGING TO RETARDED AMERICAN TIME UNITs
+        fDate = Date.parse(dateRange["start"].substr(3,2) + "/" + dateRange["start"].substr(0,2) + "/" + dateRange["start"].substr(6,4));
+        lDate = Date.parse(dateRange["end"].substr(3,2) + "/" + dateRange["end"].substr(0,2) + "/" + dateRange["end"].substr(6,4));
+        cDate = Date.parse(Mnd + "/" + Dag + "/" + Aar);
+        if((cDate <= lDate && cDate >= fDate)) {
+            return true;
+        }
+    }
+    return false
+}
 // Make the table fill the available space, while avoding scrolling of the whole pake.
 function setHeight(div) {
     var total = window.innerHeight;//$(window).height() gives different value before resize
@@ -95,74 +137,30 @@ function setHeight(div) {
     div.height(available);
     //console.log("total: "+total+", above: "+above+", below: "+below+", available: "+available);
 }
-
-function dateInRange(Aar, Mnd, Dag, yearToday, monthToday, dayToday, start, end){
-    //console.log(start == null)
-    if (start == null || end == null) {
-        if((Aar == yearToday && Mnd >= monthToday) || Aar > year){
-
-            if(Mnd == monthToday) {
-                return (Dag >= dayToday) ? true : false
-            }
-            return true
-        }
-
-    }else if (start != null && end != null){
-
-
-        var fDate,lDate,cDate;
-
-        // CHANGING TO RETARDED AMERICAN TIME UNITs
-        fDate = Date.parse(start.substr(3,2) + "/" + start.substr(0,2) + "/" + start.substr(6,4));
-        lDate = Date.parse(end.substr(3,2) + "/" + end.substr(0,2) + "/" + end.substr(6,4));
-        cDate = Date.parse(Mnd + "/" + Dag + "/" + Aar);
-
-
-        if((cDate <= lDate && cDate >= fDate)) {
-            return true;
-        }
-
-
-    }
-
-
-    return false
-}
-
-/*
-
-a function that takes in list of active schools.
-It'll hide all the schools that it doesn't need?
-
-
- */
-
-var activelist = []
-// GLOBAL VARIABLES: so it doesn't need to fetch a new reference list each time..
-function selectSchools(activeSchools) {
-
-    activelist = activeSchools
+function selectSchools(ActiveSchools) {
+    activeSchools = ActiveSchools
     // if reference list is empty, try to fetch a new one
     var listref = generateReferences()
 
 
     //iterate through the reference list
     $.each(listref, function(skoler, refs) {
-            //check if selected school is in display list,
-            // if no schools is in display list, show all schools
-        if (($.inArray(skoler, activeSchools)) != -1 || activeSchools == null || activelist.length == 0) {
+        //check if selected school is in display list,
+        // if no schools is in display list, show all schools
+        if (($.inArray(skoler, activeSchools)) != -1 || activeSchools == null) {
             // shows school if in display list or if list is empty, else hide
             $(refs).show();
-    } else {
+        } else {
+
             $(refs).hide();
         }
     })
 }
 function generateReferences() {
-    var references = {}
+
     var listref = {}
     // fetches references, as index:reference
-    references = $("#q").children();
+    var references = $("#q").children();
     // loops through each reference to fetch schoolname of that references
     $.each(references, function(index, objects) {
         // store each reference in listref with schoolname as index
@@ -171,34 +169,57 @@ function generateReferences() {
     return listref;
 }
 function daysInMonth(month,year) {
-  return new Date(year, month, 0).getDate()
+    return new Date(year, month, 0).getDate()
 }
-
-function addskolevalg(skolenavn){
-      var valg = $("<option></option>").text(skolenavn);
-     $("#skolevalg").append(valg);
-     $("#skolevalg").trigger("chosen:updated");
-}
-
 function filterDates(period){
-  // TODO: Implementer filtrering på dato av tabell
-   // console.log(period["start"])
-    //console.log(test)
-    printDays(test, period["start"], period["end"]);
-    console.log("s")
-    console.log(activelist);
-    selectSchools(activelist);
-  // perioden er dictionary av typen {start:dd/mm/yyyy, end:dd/mm/yyyy}
-  //start er første dag og end er siste dag i perioden som er valgt
-}
+    dateRange = period;
 
-// SFO/ELEV/LÆRER
+
+    //  printDays(test, period["start"], period["end"]);
+    printT()
+    // selectSchools(activeSchools);
+}
 function selectInfo(visningsType) {
-    if (visningsType == null) {
-        // print all types
-        console.log("ALL TYPES SELECTED")
+    typeList = visningsType;
+
+
+    printT()
+    //printDays(test, startRange, endRange);
+    //  selectSchools();
+}
+function cssTypes(origColour) {
+    // takes in the last entry in each freedayobject, this entry contains a .css class format: E-L-S
+    // Where E : Elev, L : Lærer, S : SFO, F: : False/filler for format
+    // Takes a list over types wanted -> Written from selectInfo
+    // Returns a string argument, type css class with background colour
+    // IFF type is not in list, will force that entry to be F,
+    // if the entire list is empty/null will act as if all types are selected
+    // Adjusts the strings from FreedayObject to match typeList
+
+    if (typeList == null) {
+        // act as if all types selected
+
+
     } else {
-        //print the types in the list
-        console.log("These types selected: " + visningsType)
+        $.each(typeList, function(index, type) {
+
+            switch(type) {
+                case "SFO":
+                    origColour =setCharAt(origColour, 4, "F")
+                    break;
+                case "Elev":
+                    origColour = setCharAt(origColour, 0, "F")
+                    break;
+                case "Lærer":
+                    origColour = setCharAt(origColour, 2, "F")
+                    break;
+
+            }
+        });
     }
+    return origColour
+}
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
 }
